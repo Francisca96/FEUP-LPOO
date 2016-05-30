0d5bd514cb0133135f25111e2c502f63a9f2c672
Francisca96 <franciscapauperio@gmail.com>
2016-05-30 13:12:54 +0100

    Incomplete - background texture

--- core/src/com/francisca/game/states/PlayState.java
+++ core/src/com/francisca/game/states/PlayState.java
@@ -17,7 +17,7 @@ public class PlayState extends State {
         super(gsm);
         pig = new Pig(50, 300);
         cam.setToOrtho(false, PiggyCoins.WIDTH / 2, PiggyCoins.HEIGHT / 2);
-        bg = new Texture("background.png");
+        bg = new Texture("bg.png");
     }
 
     @Override
@@ -36,7 +36,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bg, cam.position.x - (cam.viewportWidth  / 2), 0);
+        sb.draw(bg, 0, 0, PiggyCoins.WIDTH, PiggyCoins.HEIGHT);
         sb.draw(pig.getPig(), pig.getPosition().x, pig.getPosition().y, Pig.WIDTH, Pig.HEIGHT);
         sb.end();
     }
