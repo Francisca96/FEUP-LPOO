1fb75168506016ad88e839726719c4a06ded626d
ZeCarlosCoutinho <jczelik@gmail.com>
2016-06-02 22:57:06 +0100

    Classe Player - Adicionada
    Classe PiggyCoins
       -Array Highscores
       -Funções associadas ao Highscores

--- core/src/com/francisca/game/sprites/Pig.java
+++ core/src/com/francisca/game/sprites/Pig.java
@@ -1,7 +1,7 @@
 package com.francisca.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Francisca on 18/05/16.
@@ -10,29 +10,36 @@ public class Pig {
     public static final int WIDTH = 35;
     public static final int HEIGHT = 35;
     private static final int GRAVITY = -5;
-    private Vector3 position;
-    private Vector3 velocity;
+    private Vector2 position;
+    private Vector2 velocity;
 
     private Texture pig;
 
     public Pig(int x, int y){
-        position = new Vector3(x, y, 0);
-        velocity = new Vector3(0, 0, 0);
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        pig = new Texture("pig.png");
+    }
+
+    public Pig()
+    {
+        position = new Vector2(0, 0);
+        velocity = new Vector2(0, 0);
         pig = new Texture("pig.png");
     }
 
     public void update(float dt){
         if(position.y > 0)
-            velocity.add(0, GRAVITY, 0);
+            velocity.add(0, GRAVITY);
         velocity.scl(dt);
-        position.add(0, velocity.y, 0);
+        position.add(0, velocity.y);
         if(position.y < 0)
             position.y = 0;
 
         velocity.scl(1/dt);
     }
 
-    public Vector3 getPosition() {
+    public Vector2 getPosition() {
         return position;
     }
 
